04b69cb8dc5c47edbdab0bc19259f6d604c314b8
cigetbudi <cigetbudi@gmail.com>
2022-06-19 00:38:52 +0700

    adding new connnection Postgres

--- db/db.go
+++ db/db.go
@@ -15,6 +15,9 @@ func Init() {
 	dsn := conf.DB_USERNAME + ":" + conf.DB_PASSWORD + "@tcp(" + conf.DB_HOST + ":" + conf.DB_PORT + ")/" + conf.DB_NAME + "?charset=utf8&parseTime=true&loc=Local"
 	var err error
 	DB, err = gorm.Open(mysql.Open(dsn), &gorm.Config{SkipDefaultTransaction: true})
+
+	// dsn := "host=" + conf.DB_HOST + " user=" + conf.DB_USERNAME + " password=" + conf.DB_PASSWORD + " dbname=" + conf.DB_NAME + " port=" + conf.DB_PORT + " sslmode=disable TimeZone=Asia/Shanghai"
+	// DB, err := gorm.Open(postgres.Open(dsn), &gorm.Config{})
 	if err != nil {
 		panic("Cannot connect database")
 	}
